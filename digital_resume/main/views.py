0bc9230c2c0837f271e34ad8502644809d57ebec
Yaman Birla <yamanbirla888yb@gmail.com>
2023-04-07 10:34:57 +0530

    portfolio upated

---
+++ digital_resume/main/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+from django.contrib import messages
+from .models import (
+		UserProfile,
+		Blog,
+		Portfolio,
+		Certificate
+	)
+
+from django.views import generic
+
+
+
+class IndexView(generic.TemplateView):
+	template_name = "main/index.html"
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		
+		certificates = Certificate.objects.filter(is_active=True)
+		blogs = Blog.objects.filter(is_active=True)
+		portfolio = Portfolio.objects.filter(is_active=True)
+		
+		context["certificates"] = certificates
+		context["blogs"] = blogs
+		context["portfolio"] = portfolio
+		return context
+
+
+def ContactView(requests):
+	template_name = "main/contact.html"
+	success_url = "/"
+	return render(requests,template_name)
+
+
+class PortfolioView(generic.ListView):
+	model = Portfolio
+	template_name = "main/portfolio.html"
+	paginate_by = 10
+
+	def get_queryset(self):
+		return super().get_queryset().filter(is_active=True)
+
+
+class PortfolioDetailView(generic.DetailView):
+	model = Portfolio
+	template_name = "main/portfolio-detail.html"
+
+class BlogView(generic.ListView):
+	model = Blog
+	template_name = "main/blog.html"
+	paginate_by = 10
+	
+	def get_queryset(self):
+		return super().get_queryset().filter(is_active=True)
+
+
+class BlogDetailView(generic.DetailView):
+	model = Blog
+	template_name = "main/blog-detail.html"
